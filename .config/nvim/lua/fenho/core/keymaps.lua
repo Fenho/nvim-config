aa8a2a7cd2e79980f9c198b49ea61084d5e31558
Fenho <foarriagada@uc.cl>
2023-10-22 17:34:32 -0300

    Add initial nvim config with lazy

---
+++ .config/nvim/lua/fenho/core/keymaps.lua
@@ -0,0 +1,10 @@
+-- set leader key to space
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+---------------------
+-- General Keymaps -------------------
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
---
+++ .config/nvim/lua/fenho/plugins/colorscheme.lua
@@ -0,0 +1,7 @@
+return {
+  "caksoylar/vim-mysticaltutor",
+  priority = 1000,
+  config = function ()
+    vim.cmd([[colorscheme mysticaltutor]])
+  end,
+}
